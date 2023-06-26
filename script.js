003d9026513cf2af3580d849724408c48509d403
Oyinkansola Ojutiku <oyinkanojutiku@gmail.com>
2023-06-26 10:23:22 -0400

    create coming soon page

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
-var slideIndex = 0;
+let slideIndex = 0;
+let $slides = document.getElementsByClassName("slide");
+let $slideControlWrapper = document.getElementById("slideControlWrapper");
 
-var slides = document.getElementsByClassName("slide");
-let slideControl = document.getElementById("slideControl");
+// Create buttons for each slide
+for (i = 0; i < $slides.length; i++) {
+    // slides[i].style.display = "none";
 
+    let newB = document.createElement("button");
+    newB.addEventListener("click", showSlide);
+    newB.setAttribute("data-index", i);
+    newB.classList.add("slideControl");
 
-// for (i = 0; i < slides.length; i++) {
-//     // slides[i].style.display = "none";
+    $slideControlWrapper.appendChild(newB);
+}
 
-//     let newB = document.createElement("button");
-//     newB.addEventListener("click", showSlide);
-//     newB.setAttribute("data-index", i);
-//     slideControl.appendChild(newB);
-// }
+let $slideControls = document.getElementsByClassName("slideControl");
 
-// slides[slideIndex].style.display = "block";
+showSlide(null, slideIndex)
 
 function showSlide(e, i) {
     let n = i;
-    if (i == null) {
+    if (!i) {
         e.preventDefault();
         n = e.target.getAttribute('data-index');
     }
-    slides[slideIndex].style.display = "none";
+
+    $slides[slideIndex].style.opacity = 0;
+    $slideControls[slideIndex].classList.remove("active");
+
     slideIndex = n;
-    if (n > slides.length) { slideIndex = 0 }
-    if (n < 0) { slideIndex = slides.length -1 };
+    if (n > $slides.length) { slideIndex = 0 }
+    if (n < 0) { slideIndex = $slides.length -1 };
 
-    slides[slideIndex].style.display = "block";
+    $slides[slideIndex].style.opacity = 1;
+    $slideControls[slideIndex].classList.add("active");
 }
 
 
-carousel()
-function carousel() {
-    var i;
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.opacity = 0;
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.opacity = 1;
-    setTimeout(carousel, 5000); // Change image every 2 seconds
-}
+// carousel()
+// function carousel() {
+//     var i;
+//     for (i = 0; i < $slides.length; i++) {
+//         $slides[i].style.opacity = 0;
+//     }
+//     slideIndex++;
+//     if (slideIndex > $slides.length) { slideIndex = 1 }
+//     $slides[slideIndex - 1].style.opacity = 1;
+//     setTimeout(carousel, 5000); // Change image every 2 seconds
+// }
